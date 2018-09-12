9eefe20a50da390cc2aabe4f8f682cb84de57385
JP Lynch <JPLynch35@gmail.com>
2018-09-12 09:20:16 -0600

    create form in welcome index

--- spec/features/user_can_validate_word_spec.rb
+++ spec/features/user_can_validate_word_spec.rb
@@ -3,12 +3,14 @@ require 'rails_helper'
 describe 'a user' do
   describe 'visiting the root page' do
     it 'can validate that a word exists' do
+      visit root_path
       fill_in xxx, with: 'foxes'
       click_on 'Validate Word'
 
       expect(page).to have_content("'foxes' is a valid word and its root form is 'fox'.")
     end
     it 'can validate that a word does not exist' do
+      visit root_path
       fill_in xxx, with: 'foxez'
       click_on 'Validate Word'
 
