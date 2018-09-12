f0def35b7805916cc527deaa41ad0f9b9970aa83
JP Lynch <JPLynch35@gmail.com>
2018-09-12 09:29:43 -0600

    remove word controller, place logic in welcome index

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,6 @@
 class WelcomeController < ApplicationController
   def index
+    word = params[:word]
+    presenter = WordPresenter.new(word)
   end
 end
--- app/controllers/words_controller.rb
+++
@@ -1,6 +0,0 @@
-class WordsController < ApplicationController
-  def create
-    word = params[:word]
-    
-  end
-end
